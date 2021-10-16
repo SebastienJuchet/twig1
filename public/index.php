44f302159c452b9f6c72eb4527fdf907d4f0fd00
Sebastien Juchet <sebastien.juchet@gmail.com>
2021-10-16 17:30:49 +0200

    Finish the first quest of twig

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Get a $twig object from this file.
+require_once __DIR__ . '/../config/twig.php';
+require 'products.php';
+$name = 'Wilder';
+
+echo $twig->render('home.html.twig', ['name' => $name]);
+
+echo $twig->render('products.html.twig', ['products' => $products]);
